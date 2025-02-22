75cb909c55d9aaedaae54f2030ce530354e04113
Akilhan <oqiljondadaxanov@gmail.com>
2025-02-22 23:39:16 +0500

    hero initialized

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ARBIChO - International Abu Reikhan Beruniy Olympiad",
+  description:
+    "Join the prestigious International Abu Reikhan Beruniy Chemistry Olympiad and showcase your chemistry prowess on a global stage.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
