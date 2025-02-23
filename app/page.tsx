26bf3d620126b918bb0bbfb863ca3187185f4c66
Okiljon <okiljondadakhonov@gmail.com>
2025-02-23 14:41:58 +0500

    feat: new sections added(competetion, footer, sponsors and timeline)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { CompetitionSection } from "@/components/competetion";
 import Hero from "@/components/hero";
+import { SponsorsSection } from "@/components/sponsors";
+import { TimelineSection } from "@/components/timeline";
 
 export default function Home() {
   return (
     <section>
       <Hero />
+      <CompetitionSection />
+      <TimelineSection />
+      <SponsorsSection />
     </section>
   );
 }
